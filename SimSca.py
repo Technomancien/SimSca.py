6632d3d25976bf1cb655eca5c68f722e8d336df0
Technomancien <35791034+Technomancien@users.noreply.github.com>
2018-01-24 23:16:33 -0500

    Add files via upload

---
+++ SimSca.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+
+## [Title]: SimSca.py  -- recon/enumeration script
+## [Author]: FG
+##-------------------------------------------------------------------------------------------------------------
+## [Details]: Simple Nmap vuln scanner
+## 
+##	Utilisation: python OPscan.py xxx.xxx.xxx.xxx
+##-------------------------------------------------------------------------------------------------------------
+
+print "############################################################"
+print "####                 		                          ####"
+print "####                   OP RECON SCAN 		          ####"
+print "####         					                      ####"
+print "############################################################"
+
+'requirement : nmap,xsltproc and vulners.nse'
+
+import sys
+import os
+import subprocess
+
+
+
+
+if len(sys.argv) != 2:
+   print " Put a IP address !!!"
+   sys.exit(0)
+
+
+
+ip_address = sys.argv[1] 
+print "INFO: python SimSca.py " + ip_address
+print "Go get a cofee it might take some time ..."
+SCAN = "nmap -vv -sS -Pn -A -sV -T4 --script=default,auth,vuln,discovery,vulners -p- -oX scan.xml --script-args=safe=1 %s" %(ip_address) 
+results = subprocess.check_output(SCAN, shell=True) 
+
+if results != "":
+	Transfert = "xsltproc scan.xml -o " + ip_address + "_scan.htm"
+	results2 = subprocess.check_output(Transfert, shell=True)
+	os.remove('scan.xml')
+
+print "###############################################################################"
+
+print "INFO: Nmap scans completed for "  + ip_address 
+
+print "###############################################################################"
+
+ 
+
